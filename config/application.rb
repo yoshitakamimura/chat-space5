b183b1add15d4865a00acfb5f88a58fdf2176f18
yoshitakamimura <kix11974@yahoo.co.jp>
2020-06-13 19:52:16 +0900

    chat-spaceのflont実装

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,11 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace5
   class Application < Rails::Application
-    config.load_defaults 6.0
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+    end
   end
 end
