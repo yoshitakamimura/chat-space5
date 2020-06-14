83c0e3671cd9fb8d9ad89532f17405b2cf215380
yoshitakamimura <kix11974@yahoo.co.jp>
2020-06-14 22:02:41 +0900

    メッセージ送信機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :name, presence: true, uniqueness: true
-
+  has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
 end
