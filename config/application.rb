9feca08edb2ffa963e09fd91800541aa5d87735a
yoshitakamimura <kix11974@yahoo.co.jp>
2020-06-16 11:49:49 +0900

    非同期通信の実装

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ module ChatSpace5
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja
     end
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
